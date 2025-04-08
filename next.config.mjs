7f463403895bb7d77099033e905c4529e69c87f4
Willy Silva <i_struck@hotmail.com>
2025-04-08 14:15:39 -0300

    Configure TypeScript and ESLint settings in next.config.mjs to ignore build errors

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   output: 'standalone',
   experimental: {
     serverComponentsExternalPackages: ['faiss-node']
+  },
+  typescript: {
+    // ⚠️ Warning: This allows production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // ⚠️ Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   }
 };
 
